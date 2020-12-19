ad6879b8b8668fa110348e8b9006684bef4aa717
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-19 07:46:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array){
+  return array.map(elem => elem * -1)
+}
+function mapOriginalValue(array){
+  return array.map(elem => elem)
+}
+function mapValueTimes2(array){
+  return array.map(elem => elem * 2)
+}
+function mapToSquare(array){
+  return array.map(elem => elem ** 2)
+}
+//mapToSquare([1, 2, 3, -9])
+function reduceToOne(array){
+  return array.reduce((acc,curr) => acc+curr)
+}
